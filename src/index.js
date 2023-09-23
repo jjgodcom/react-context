4e1fbed78b31e08359c6d27ed8782e065d977f5f
jjgodcom <jjgodcom1@gamil.com>
2023-09-23 17:50:31 +0900

    sass 설치 및 세팅

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import "./assets/scss/style.scss"; // 여기에 선언하면 전역에서 스타일 사용 가능
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
